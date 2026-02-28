88418285f31ce5165124749630fbbea6e49f0cb4
leandrinm1 <tu-correo@ejemplo.com>
2026-02-28 16:43:16 -0500

    Fix final: Forzando Node 20 y transpile de undici

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  transpilePackages: ['undici', 'firebase', '@firebase/functions'],
   experimental: {
-    serverActions: {
-      allowedOrigins: ["localhost:3000", process.env.NEXT_PUBLIC_APP_URL ?? ""],
-    },
-  },
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "firebasestorage.googleapis.com" },
-      { protocol: "https", hostname: "lh3.googleusercontent.com" },
-    ],
-  },
-};
-
-module.exports = nextConfig;
+    esmExternals: 'loose'
+  }
+}
